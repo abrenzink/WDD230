7b784f015b2a3f30f7dcb7642e7e56f4e6452cf0
abrenzink <a.brenzink@gmail.com>
2022-01-28 15:10:41 -0400

    friday

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -24,12 +24,28 @@ const months = [
 ];
 
 const d = new Date();
-
 const dayName = daynames[d.getDay()];
 const monthName = months[d.getMonth()];
 const year = d.getFullYear();
 
 const fulldate = `${dayName}, ${d.getDay()} ${monthName} ${year}`;
+const lastUpdate = document.lastModified;
 
 
-document.getElementById("currentDate").textContent = fulldate;
+document.getElementById("date").textContent = year;
+
+
+
+const x = getElementById("hamBtn");
+x.onclick = toggleMenu();
+
+
+function toggleMenu(){
+	//here classList specifies what property of the chosen element
+	//we want to afect. toggle is a key word that makes the job of
+	//toggling the "responsive" name into the classList property.
+	
+
+	console.log("WORKED!!")
+	//document.getElementsByClassName("navigation").classList.toggle("open");
+}
