27cd1f1260463aa00a8e2e7b6226cfeeea53cbf9
abrenzink <a.brenzink@gmail.com>
2022-02-19 15:39:45 -0400

    days

--- lesson7/chamber/js/script.js
+++ lesson7/chamber/js/script.js
@@ -104,21 +104,3 @@ function toggleMenu(){
 	document.getElementsByClassName("navigation").classList.toggle("open");
 }
 
-/*----------------------------------DAYS------------------------------------*/
-
-const numDays = document.querySelector(".numDays");
-
-let lastVisit = Number(window.localStorage.getItem("lastVisit"))
-
-if (lastVisit !== 0) {
-	numDays.textContent = `This is your first visit!`
-	console.log(numDays)
-} else {
-	let thisVisit = d.now();
-	let timeDiff = Math.abs(thisVisit.getTime() - lastVisit.getTime());
-	let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-	console.log(diffDays)
-	numDays.textContent = `${diffDays}`
-}
-
-localStorage.setItem("lastVisit", JSON.stringify(d.now()));
