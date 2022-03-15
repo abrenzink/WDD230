7d230c97657388d9289528ee2a5811ec311a7edd
abrenzink <a.brenzink@gmail.com>
2022-03-15 13:02:28 -0400

    time

--- lesson10/chamber/js/localStorage.js
+++ lesson10/chamber/js/localStorage.js
@@ -2,15 +2,16 @@ const numDays = document.querySelector("#numDays");
 
 let lastVisit = Number(window.localStorage.getItem("lastVisit"))
 
-if (lastVisit !== 0) {
+if (lastVisit === 0) {
 	numDays.textContent = `This is your first visit!`
-	console.log(numDays)
+	
 } else {
 	let thisVisit = Date.now();
-	let timeDiff = Math.abs(thisVisit.getTime() - lastVisit.getTime());
-	let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-	console.log(diffDays)
-	numDays.textContent = `${diffDays}`
+	let timeDiff = thisVisit - lastVisit;
+	//let diffDays = timeDiff / 1000;  this is for seconds
+	
+	let diffDays = timeDiff / (1000 * 3600 * 24); 
+	numDays.textContent = `${diffDays.toFixed(1)}`
 }
 
 localStorage.setItem("lastVisit", JSON.stringify(Date.now()));
